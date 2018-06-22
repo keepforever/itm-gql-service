ee549ed62616177f0c02e21bb5688d226fdd955e
Brian Cilenti <briancilenti@gmail.com>
2018-06-22 12:32:04 -0700

    added error logic to auth mutation

--- src/resolvers/Mutation/auth.js
+++ src/resolvers/Mutation/auth.js
@@ -17,17 +17,35 @@ const auth = {
   async login(parent, { email, password }, ctx, info) {
     const user = await ctx.db.query.user({ where: { email } })
     if (!user) {
-      throw new Error(`No such user found for email: ${email}`)
+      // return error replaces throw new Error(..)
+      //throw new Error(`No such user found for email: ${email}`)
+      return {
+        error: {
+          field: 'email',
+          msg: 'No user found'
+        }
+      }
     }
 
     const valid = await bcrypt.compare(password, user.password)
     if (!valid) {
-      throw new Error('Invalid password')
+      // returning error object replaces throw new Error('..')
+      //throw new Error('Invalid password')
+      return {
+        error: {
+          field: 'email',
+          msg: 'Invalid Password'
+        }
+      }
     }
 
     return {
-      token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
-      user,
+      //Can't just return token and user, need to wrap in payload
+      //for error message display on client.
+      payload: {
+        token: jwt.sign({ userId: user.id }, process.env.APP_SECRET),
+        user,
+      }
     }
   },
 }
