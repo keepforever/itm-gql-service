b3559cd9ca87015ce00a7a7cd4614cd5ac575587
Brian Cilenti <briancilenti@gmail.com>
2018-06-24 21:54:41 -0700

    added offer type and mutation. Here goes nothing...

---
+++ src/resolvers/Mutation/offer.js
@@ -0,0 +1,34 @@
+const { getUserId } = require('../../utils')
+
+const offer = {
+  async createOffer(parent, { title, text }, ctx, info) {
+    const userId = getUserId(ctx)
+    return ctx.db.mutation.createPost(
+      {
+        data: {
+          title,
+          text,
+          author: {
+            connect: { id: userId },
+          },
+        },
+      },
+      info
+    )
+  },
+
+  async deleteOffer(parent, { id }, ctx, info) {
+    const userId = getUserId(ctx)
+    const offerExists = await ctx.db.exists.Offer({
+      id,
+      author: { id: userId },
+    })
+    if (!offerExists) {
+      throw new Error(`Offer not found or you're not the author`)
+    }
+
+    return ctx.db.mutation.deleteOffer({ where: { id } })
+  },
+}
+
+module.exports = { post }
